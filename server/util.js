0393a4a87c5017753af509be017b02473158ab25
Edivando Severo <edivando@grupofortes.com.br>
2022-03-15 14:06:58 -0300

    Adição de modulo para adequação de variaveis em ambiente de desenvolvimento

--- server/util.js
+++ server/util.js
@@ -1,7 +1,7 @@
 import pino from "pino";
 
 const log = pino({
-  enabled: false, //!(!!process.env.LOG_DISABLED),
+  enabled: !(!!process.env.LOG_DISABLED),
   transport: {
     target: "pino-pretty",
     options: {
